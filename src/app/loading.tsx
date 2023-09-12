d91261f740eb4f47f0e9a02940903a8b04d0ab8f
sherland <vancklies@gmail.com>
2023-09-13 02:52:38 +0300

    Начато добавление формы регистрации

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/styles/loading.module.css"
-
-export default function Loading() {
-    return (
-        <div className={styles.loading}>
-            <div className={styles.loading__circle}></div>
-        </div>
-    )
-}
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -3,7 +3,13 @@ import styles from "@/styles/signUp.module.css"
 export default function signUp() {
     return (
         <div className={styles.signUp}>
-            <div>Регистрация</div>
+            <form className={styles.signUp__form}>
+                <label>Sign up</label>
+                <input type="text" placeholder="Enter your email" />
+                <input type="password" placeholder="Create a password" />
+                <input type="password" placeholder="Confirm your password" />
+                <button type="submit">Sign up</button>
+            </form>
         </div>
     )
 }
